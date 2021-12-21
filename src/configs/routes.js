febe6ed6d8f89b944fcad88d0f3b7631edb9cedf
fadzrinmadu <muhfadzrinmadu@gmail.com>
2021-12-21 17:17:20 +0700

    delete gallery routes

--- src/configs/routes.js
+++ src/configs/routes.js
@@ -1,7 +1,5 @@
 const breedsRoutes = require('../api/breeds/routes');
-const galleriesRoutes = require('../api/galleries/routes');
 
 module.exports = (app) => {
   app.use('/api/v1', breedsRoutes);
-  app.use('/api/v1', galleriesRoutes);
 };
